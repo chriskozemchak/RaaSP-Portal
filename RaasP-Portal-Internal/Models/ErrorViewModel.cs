0e1c8965b466d74365f38788f006d94fe5dd5734
chriskozemchak <chriskozemchak@gwu.edu>
2018-10-02 10:16:26 -0400

    Initial Layout Changes

---
+++ RaasP-Portal-Internal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RaasP_Portal_Internal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
